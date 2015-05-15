0c7982b76e9eea0335c95dca9a4378640e3d9625
yibrahim <ymibrahi@uwaterloo.ca>
2015-05-15 17:35:00 -0400

    Returns all duplicates within a list of integers

--- Find all duplicates in list.py
+++ Find all duplicates in list.py
@@ -1,16 +1,44 @@
 # Given a list of integers, return a list of all duplicates 
+def find_dups(int_array):
+	# Checks base case of empty list
+	if not int_array:
+		return None
 
-def find_dups(array_of_ints):
-	numbers = list(len(array_of_ints))
-	duplicates = []
+	# Sort the array to group all duplicates
+	int_array.sort()
 
-	for num in array_of_ints:
-		if numbers[num] = 1:
-			duplicates.append(num)
-		else:
-			numbers[num] = 1
+	# Create an array of size two to shift 
+	# integers into
+	dups = [0]*2
 
-	# Solution can be extended to return only one of
-	# each duplicate found by:
-    # return list(set(duplicates))
-	return duplicates
+	# Create array to store duplicate values
+	dup=[]
+
+	# Copy first two integers into array
+	dups[0:2] = int_array[0:2]
+
+	# Iterate through array of integers,
+	# and check for any duplicates
+	for num in range(0,len(int_array)-1):
+		if dups[0] == dups[1]:
+			dup.append(dups[0])
+
+		# Shift the next integer
+		dups[0:2] = int_array[num:num+2]
+
+	# If there are duplicates, then return sorted list
+	# with only one duplicate for each integer using a set
+	if len(dup)>1:
+		return list(set(dup.sort()))
+	
+	elif len(dup)==1:
+		return dup
+	
+	else:
+		return None
+
+
+
+import pdb
+pdb.set_trace()
+find_dups([1,1])
